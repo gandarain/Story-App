ea3e0df08d7ea1c644f96e0970446a1892d3d9e5
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-11 22:03:42 +0700

    #### Enhance UI Login

--- app/src/main/java/com/dicoding/storyapp/LoginActivity.kt
+++ app/src/main/java/com/dicoding/storyapp/LoginActivity.kt
@@ -1,29 +1,17 @@
 package com.dicoding.storyapp
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowInsets
-import android.view.WindowManager
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        setupView()
+        
+        hideActionBar()
     }
 
-    private fun setupView() {
-        @Suppress("DEPRECATION")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        } else {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
-        }
+    private fun hideActionBar() {
         supportActionBar?.hide()
     }
 }
