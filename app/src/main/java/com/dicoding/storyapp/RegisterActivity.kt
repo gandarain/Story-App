0b91bbb6c79e570310d2818e7a71a952a5650db7
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-13 22:17:27 +0700

    #### Action Bar Register

--- app/src/main/java/com/dicoding/storyapp/RegisterActivity.kt
+++ app/src/main/java/com/dicoding/storyapp/RegisterActivity.kt
@@ -2,10 +2,23 @@ package com.dicoding.storyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        title = resources.getString(R.string.register)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return true
     }
 }
