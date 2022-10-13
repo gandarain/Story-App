0032cf7dd0ada8fe8772224a69449494c71244d3
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-13 22:10:40 +0700

    #### Register Layout

--- app/src/main/java/com/dicoding/storyapp/LoginActivity.kt
+++ app/src/main/java/com/dicoding/storyapp/LoginActivity.kt
@@ -1,17 +1,31 @@
 package com.dicoding.storyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.storyapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         
         hideActionBar()
+
+        registerButtonHandler()
     }
 
     private fun hideActionBar() {
         supportActionBar?.hide()
+    }
+
+    private fun registerButtonHandler() {
+        binding.registerButton.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
