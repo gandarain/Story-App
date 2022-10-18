cae43030810318e2a1a8f332fbb4bcd16b97712f
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-18 22:51:48 +0700

    #### Get Story on Home Fragment

---
+++ app/src/main/java/com/dicoding/storyapp/model/StoryResponse.kt
@@ -0,0 +1,41 @@
+package com.dicoding.storyapp.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class StoryResponse(
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("listStory")
+    val listStory: Story
+): Parcelable
+
+@Parcelize
+data class Story(
+    @field:SerializedName("id")
+    val id: String,
+
+    @field:SerializedName("name")
+    val name: String,
+
+    @field:SerializedName("description")
+    val description: String,
+
+    @field:SerializedName("photoUrl")
+    val photoUrl: String,
+
+    @field:SerializedName("createdAt")
+    val createdAt: String,
+
+    @field:SerializedName("lat")
+    val lat: Float,
+
+    @field:SerializedName("lon")
+    val lon: Float,
+): Parcelable
--- app/src/main/java/com/dicoding/storyapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/dicoding/storyapp/ui/home/HomeViewModel.kt
@@ -1,13 +1,58 @@
 package com.dicoding.storyapp.ui.home
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.dicoding.storyapp.api.ApiConfig
+import com.dicoding.storyapp.model.Story
+import com.dicoding.storyapp.model.StoryResponse
+import com.dicoding.storyapp.preference.LoginPreference
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(application: Application) : AndroidViewModel(application) {
+    private val context = getApplication<Application>().applicationContext
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _listStories = MutableLiveData<Story>()
+    val listStories: LiveData<Story> = _listStories
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _isError = MutableLiveData<Boolean>()
+    val isError: LiveData<Boolean> = _isError
+
+    init {
+        getStories()
     }
-    val text: LiveData<String> = _text
+
+    private fun getStories() {
+        val token = LoginPreference(context).getUser().token
+        _isError.value = false
+        _isLoading.value = true
+
+        val client = token?.let {
+            ApiConfig.getApiService().getStories(token = "Bearer $it", page = 1, size = 100, location = 0)
+        }
+
+        client?.enqueue(object : Callback<StoryResponse> {
+            override fun onResponse(call: Call<StoryResponse>, response: Response<StoryResponse>) {
+                if (response.body()?.error == false) {
+                    _listStories.value = response.body()?.listStory
+                    _isLoading.value = false
+                    _isError.value = false
+                } else {
+                    _isError.value = false
+                    _isLoading.value = false
+                }
+            }
+
+            override fun onFailure(call: Call<StoryResponse>, t: Throwable) {
+                _isLoading.value = false
+                _isError.value = true
+            }
+        })
+    }
 }
