b9adb047017dbd5391959431a333efaf82314499
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-20 22:17:55 +0700

    #### Create a new Activity for Create Story

---
+++ app/src/main/java/com/dicoding/storyapp/ui/create_story/CreateStoryActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.storyapp.ui.create_story
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_story)
+    }
+}
