b9a7ab1f25ae9acbedfde90cfdd31f9263964f72
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-20 22:28:59 +0700

    #### Enhance Toolbar Create Story Screen

--- app/src/main/java/com/dicoding/storyapp/ui/create_story/CreateStoryActivity.kt
+++ app/src/main/java/com/dicoding/storyapp/ui/create_story/CreateStoryActivity.kt
@@ -2,11 +2,24 @@ package com.dicoding.storyapp.ui.create_story
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.dicoding.storyapp.R
 
 class CreateStoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_story)
+
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        title = resources.getString(R.string.create_story)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return true
     }
 }
