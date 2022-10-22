0aada20ef83fd1f315d212a32f57a33d5cd67bc0
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-22 13:07:34 +0700

    #### Permission Handler

--- app/src/main/java/com/dicoding/storyapp/constants/Constants.kt
+++ app/src/main/java/com/dicoding/storyapp/constants/Constants.kt
@@ -2,4 +2,6 @@ package com.dicoding.storyapp.constants
 
 object Constants {
     const val SPLASH_SCREEN_TIMER: Long = 3000
+    const val REQUEST_CODE_PERMISSIONS = 10
+    val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
 }
--- app/src/main/java/com/dicoding/storyapp/ui/create_story/CreateStoryActivity.kt
+++ app/src/main/java/com/dicoding/storyapp/ui/create_story/CreateStoryActivity.kt
@@ -1,16 +1,26 @@
 package com.dicoding.storyapp.ui.create_story
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.dicoding.storyapp.R
+import com.dicoding.storyapp.constants.Constants
+import com.dicoding.storyapp.databinding.ActivityCreateStoryBinding
 
 class CreateStoryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateStoryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_story)
+        binding = ActivityCreateStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setupToolbar()
+        checkPermission()
     }
 
     private fun setupToolbar() {
@@ -21,5 +31,33 @@ class CreateStoryActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         finish()
         return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionGranted()) {
+                Toast.makeText(this@CreateStoryActivity, R.string.permission_denied, Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionGranted() = Constants.REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun checkPermission() {
+        if (!allPermissionGranted()) {
+            ActivityCompat.requestPermissions(
+                this@CreateStoryActivity,
+                Constants.REQUIRED_PERMISSIONS,
+                Constants.REQUEST_CODE_PERMISSIONS
+            )
+        }
     }
 }
