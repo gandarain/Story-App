f8bead40ad30defff228e8d643c2e30540dcf7cd
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-23 15:40:55 +0700

    #### Detail Story Layout

--- app/src/main/java/com/dicoding/storyapp/ui/detail_story/DetailStoryActivity.kt
+++ app/src/main/java/com/dicoding/storyapp/ui/detail_story/DetailStoryActivity.kt
@@ -2,12 +2,13 @@ package com.dicoding.storyapp.ui.detail_story
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
+import com.bumptech.glide.Glide
 import com.dicoding.storyapp.R
 import com.dicoding.storyapp.constants.Constants
 import com.dicoding.storyapp.databinding.ActivityDetailStoryBinding
 import com.dicoding.storyapp.model.Story
+import com.dicoding.storyapp.utils.withDateFormat
 
 class DetailStoryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailStoryBinding
@@ -17,10 +18,10 @@ class DetailStoryActivity : AppCompatActivity() {
         binding = ActivityDetailStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val detailStory = intent.getSerializableExtra(Constants.DETAIL_STORY) as? Story
-        Log.d("DetailStoryActivity", detailStory.toString())
+        val detailStory = intent.getParcelableExtra<Story>(Constants.DETAIL_STORY) as Story
 
         setupToolBar()
+        setupUi(detailStory)
     }
 
     private fun setupToolBar() {
@@ -31,5 +32,18 @@ class DetailStoryActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         finish()
         return true
+    }
+
+    private fun setupUi(detailStory: Story) {
+        Glide.with(this@DetailStoryActivity)
+            .load(detailStory.photoUrl)
+            .fitCenter()
+            .into(binding.storyImageView)
+
+        detailStory.apply {
+            binding.nameTextView.setText(name)
+            binding.descriptionTextView.setText(description)
+            binding.dateTextView.setText(createdAt.withDateFormat())
+        }
     }
 }
