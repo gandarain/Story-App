0d6ce31398e61a34b1ab0006bac14e10977ef904
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-10-24 21:47:28 +0700

    #### Create Custom Button

---
+++ app/src/main/java/com/dicoding/storyapp/custom_view/CustomButton.kt
@@ -0,0 +1,38 @@
+package com.dicoding.storyapp.custom_view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import androidx.core.content.ContextCompat
+import com.dicoding.storyapp.R
+import com.google.android.material.button.MaterialButton
+
+class CustomButton : MaterialButton {
+
+    private lateinit var enabledBackground: Drawable
+    private lateinit var disabledBackground: Drawable
+    private var txtColor: Int = 0
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        background = if(isEnabled) enabledBackground else disabledBackground
+    }
+    private fun init() {
+        txtColor = ContextCompat.getColor(context, android.R.color.background_light)
+        enabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button) as Drawable
+        disabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button_disabled) as Drawable
+    }
+}
